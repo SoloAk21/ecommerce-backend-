b524af360ee2414562e48f9d40ce8f9491f116da
Solomon-Asefa <soloasefa6603@gmail.com>
2025-08-23 23:57:14 +0300

    Phase 2: Sequelize configuration and database connection

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const express = require("express");
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
+  }
+);
+
 app.get("/", (req, res) => {
   res.send("E-Commerce Backend is running!");
 });
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.error("Unable to connect to the database:", err));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
